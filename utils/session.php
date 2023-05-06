5e371d88c60de65d70bb025b9e9c21a8e16a2a7c
ze <joseapmartins2003@gmail.com>
2023-05-06 18:45:17 +0100

    Navbar done. Just needs to be improved and to be responsive to smartphones

---
+++ utils/session.php
@@ -0,0 +1,53 @@
+<?php
+
+class Session {
+
+    public function __construct($userType) {
+        session_start();
+        $_SESSION['userType'] = $userType;
+    }
+
+    public function isLoggedIn() {
+        //return isset($_SESSION['email']);
+        return true;
+    }
+
+    public function loggout() {
+        session_destroy();
+    }
+
+    public function getUsername() {
+        return isset($_SESSION['username']) ? $_SESSION['username'] : null;
+    }
+
+    public function getEmail() {
+        return isset($_SESSION['email']) ? $_SESSION['email'] : null;
+    }
+
+    public function getUserType() {
+        return isset($_SESSION['userType']) ? $_SESSION['userType'] : null;
+    }
+
+    public function setEmail($email) {
+        $_SESSION['email'] = $email;
+    }
+
+    public function setUsername($username) {
+        $_SESSION['username'] = $username;
+    }
+
+    public function setUserType($userType) {
+        $_SESSION['userType'] = $userType;
+    }
+
+    public function isStaff() {
+        return ($_SESSION['userType'] == "Staff" || $_SESSION['userType'] == "Admin") ? true : false;
+    }
+
+    public function isAdmin() {
+        return ($_SESSION['userType'] == "Admin") ? true : false;
+    }
+
+}
+
+?>
