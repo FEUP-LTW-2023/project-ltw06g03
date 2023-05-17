85bf2693faa31348987e895f5d22107d06c9c8cf
FranciscoCardoso913 <franciscocardoso.3003@gmail.com>
2023-05-17 09:33:50 +0100

    cleaning up code

--- javascript/script.js
+++
@@ -1,7 +0,0 @@
-export function disableScroll(){
-    const body =document.querySelector('body');
-    body.style.overflow='hidden';
-    const elment= document.createElement('a');
-    elment.innerText='Ola bitch';
-    return elment;
-}
