a6deac096cb8ebd638d833c48ca0afe3aa629d9b
FranciscoCardoso913 <franciscocardoso.3003@gmail.com>
2023-05-20 09:21:00 +0100

    Fixed responsive bug in user page

--- pages/profile.php
+++ pages/profile.php
@@ -1,25 +1,28 @@
 <?php
-    declare(strict_types=1);
-    require_once('../utils/session.php');
-    require_once('../templates/header.php');
-    require_once('../templates/common.php');
-    require_once('../templates/body.php');
-    $session = new Session();
 
-    $session->setDateOfRegister("2020-20-02");
+require_once('../utils/session.php');
+require_once('../templates/header.php');
+require_once('../templates/common.php');
+require_once('../templates/user_body.php');
 
-    if (!$session->isLoggedIn()) {
-        header('Location: home.php');
-    }
-    if(!isset($_GET['up']))header('Location: profile.php?up='. $session->getUp());
 
-    else {
-        drawProfileHeader($session);
-        drawNavBar($session);
-        drawProfileBody($session);
-        drawFooter();
+//set Session
+$session = new Session();
 
-    }
+$session->setDateOfRegister("2020-20-02");
+
+if (!$session->isLoggedIn()) {
+    header('Location: home.php');
+}
+if(!isset($_GET['up']))header('Location: profile.php?up='. $session->getUp());
+
+else {
+    drawProfileHeader($session);
+    drawNavBar($session);
+    drawUserBody($session);
+    drawFooter();
+
+}
 
 
 ?>
--- templates/user_body.php
+++ templates/user_body.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 function drawUserBody($session) {
     require_once('../templates/tickets.php');
     require_once ("../database/user.class.php");
     require_once ("../database/connection.php");
 
-   $up=intval($_GET['up']);
+    $up=intval($_GET['up']);
 
 
 
@@ -21,45 +21,45 @@ function drawUserBody($session) {
     ?>
     <div class="user-page">
 
-    <div class="user">
-        <div class="user-icon-info">
-            <?php 
-            if ($userImg != null) {
-                echo '<img src="' . $userImg . '" alt="user-img">' ;
-            }
-            else { ?>
-                <i class="fas fa-user"></i>
-            <?php } ?>
+        <div class="user">
+            <div class="user-icon-info">
+                <?php
+                if ($userImg != null) {
+                    echo '<img src="' . $userImg . '" alt="user-img">' ;
+                }
+                else { ?>
+                    <i class="fas fa-user"></i>
+                <?php } ?>
 
-            <h2> <?php echo $username ?> </h2>
-            <h3> up<?php echo $up ?> </h3>
-            <h4> <?php echo $userType ?> </h4>
-        </div>
+                <h2> <?php echo $username ?> </h2>
+                <h3> up<?php echo $up ?> </h3>
+                <h4> <?php echo $userType ?> </h4>
+            </div>
 
-        <div class="user-info">
-            <h4> User Email: <?php echo $userEmail ?> </h4>
-            <h4> This user joined at: <?php echo $date ?> </h4>
-            <?php if(count($userDepartments)>0){ ?>
-            <h4> Departments: </h4>
-            <ul>
-            <?php if ($userType != 'Student') {
-                for ($i = 0; $i < count($userDepartments); $i++) { ?>
-                    <li><h4> <?php echo $userDepartments[$i]?> </h4></li>
-            <?php }
-            } ?>
-            </ul>
-            <?php } ?>
+            <div class="user-info">
+                <h4> User Email: <?php echo $userEmail ?> </h4>
+                <h4> This user joined at: <?php echo $date ?> </h4>
+                <?php if(count($userDepartments)>0){ ?>
+                    <h4> Departments: </h4>
+                    <ul>
+                        <?php if ($userType != 'Student') {
+                            for ($i = 0; $i < count($userDepartments); $i++) { ?>
+                                <li><h4> <?php echo $userDepartments[$i]?> </h4></li>
+                            <?php }
+                        } ?>
+                    </ul>
+                <?php } ?>
+            </div>
+            <?php if($user_up===$up){ ?>
+                <div class="links">
+                    <?php
+                    echo '<a href="../pages/tickets.php?up=' . $up . '"> User Tickets <i class="fas fa-ticket-alt"></i></a>';
+                    ?>
+                    <a href ="../actions/logout.php"> Sign Out <i class="fas fa-sign-out-alt"></i></a>
+                    <a href="../pages/edit.php?up=<?php echo $up ?>"> Edit Info <i class="fas fa-edit"></i></a>
+                </div>
+            <?php }?>
         </div>
-        <?php if($user_up===$up){ ?>
-        <div class="links">
-            <?php
-                echo '<a href="../pages/tickets.php?up=' . $up . '"> User Tickets <i class="fas fa-ticket-alt"></i></a>';
-            ?>
-            <a href ="../actions/logout.php"> Sign Out <i class="fas fa-sign-out-alt"></i></a>
-            <a href="../pages/edit.php?up=<?php echo $up ?>"> Edit Info <i class="fas fa-edit"></i></a>
-        </div>
-        <?php }?>
-    </div>
     </div>
 <?php } ?>
 
