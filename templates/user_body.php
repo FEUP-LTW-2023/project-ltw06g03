e9187ba8f013def485fe9132865d9e4428cedc37
ze <joseapmartins2003@gmail.com>
2023-05-21 21:56:45 +0100

    Small refactoring

--- templates/user_body.php
+++ templates/user_body.php
@@ -31,20 +31,20 @@ function drawUserBody($session) {
                     <i class="fas fa-user"></i>
                 <?php } ?>
 
-                <h2> <?php echo $username ?> </h2>
-                <h3> up<?php echo $up ?> </h3>
-                <h4> <?php echo $userType ?> </h4>
+                <h2> <?= $username ?> </h2>
+                <h3> up<?= $up ?> </h3>
+                <h4> <?= $userType ?> </h4>
             </div>
 
             <div class="user-info">
-                <h4> User Email: <?php echo $userEmail ?> </h4>
-                <h4> This user joined at: <?php echo $date ?> </h4>
+                <h4> User Email: <?= $userEmail ?> </h4>
+                <h4> This user joined at: <?= $date ?> </h4>
                 <?php if(count($userDepartments)>0){ ?>
                     <h4> Departments: </h4>
                     <ul>
                         <?php if ($userType != 'Student') {
                             for ($i = 0; $i < count($userDepartments); $i++) { ?>
-                                <li><h4> <?php echo $userDepartments[$i]?> </h4></li>
+                                <li><h4> <?= $userDepartments[$i]?> </h4></li>
                             <?php }
                         } ?>
                     </ul>
@@ -56,7 +56,7 @@ function drawUserBody($session) {
                     echo '<a href="../pages/tickets.php?up=' . $up . '"> User Tickets <i class="fas fa-ticket-alt"></i></a>';
                     ?>
                     <a href ="../actions/logout.php"> Sign Out <i class="fas fa-sign-out-alt"></i></a>
-                    <a href="../pages/edit.php?up=<?php echo $up ?>"> Edit Info <i class="fas fa-edit"></i></a>
+                    <a href="../pages/edit.php?up=<?= $up ?>"> Edit Info <i class="fas fa-edit"></i></a>
                 </div>
             <?php }?>
         </div>
